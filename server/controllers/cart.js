a0187df16ddad74161c5f5491dabe3f8f314d5c0
Giada <alduenda.gia@gmail.com>
2022-09-29 17:36:57 -0400

    Update cart

--- server/controllers/cart.js
+++ server/controllers/cart.js
@@ -3,35 +3,12 @@ const { Sequelize } = require("sequelize");
 const db = require('../lib/db')
 const { getShoppingCart } = require('./helpers')
 
-const mapDbToApiModel = (dbModel) => {
-    return dbModel.reduce((acc, cartItem) => {
-        let resultCartItem = acc.find(innerCartItem => innerCartItem.inventoryId === cartItem.inventory_id)
-        if (!resultCartItem) {
-            resultCartItem = {
-                itemIds: [],
-                name: cartItem.name,
-                price: cartItem.price,
-                imageUrl: cartItem.image_url,
-                size: cartItem.size,
-                inventoryId: cartItem.inventory_id,
-                quantity: 0,
-            }
-            acc.push(resultCartItem)
-        }
-
-        resultCartItem.itemIds.push(cartItem.id)
-        resultCartItem.quantity++
-        return acc
-    }, [])
-
-}
-
 module.exports = {
     getShoppingCart: (req, res) => {
-        //TODO: need to use userId from Firebase auth 
-        getShoppingCart('dean').then(dbRes => {
-            const resBody = mapDbToApiModel(dbRes)
-            res.status(200).send(resBody)
+        const userId = req.authenticatedUser.uid
+
+        getShoppingCart(userId).then(cart => {
+            res.status(200).send(cart)
         }).catch(err => {
             console.log('error', err)
             res.sendStatus(500)
@@ -39,23 +16,25 @@ module.exports = {
     },
 
     addToCart: (req, res) => {
+        const userId = req.authenticatedUser.uid
+
         const { inventoryId } = req.body;
         db.query(`
         INSERT INTO public.cart(
             inventory_id, user_id)
-           VALUES (  ${inventoryId}, 'dean');
+           VALUES (  ${inventoryId}, '${userId}');
         `, { type: Sequelize.QueryTypes.INSERT }).then(dbRes => res.status(200).send(dbRes)).catch(err => {
             console.log('error', err)
             res.sendStatus(500)
         })
-    }
-    ,
+    },
 
     removeFromCart: (req, res) => {
+        const userId = req.authenticatedUser.uid
         const { id } = req.params;
         db.query(`
         delete from cart
-        where ${id} = id
+        where ${id} = id and ${userId} = user_id
         `, { type: Sequelize.QueryTypes.DELETE }).then(dbRes => res.status(200).send(dbRes)).catch(err => {
             console.log('error', err)
             res.sendStatus(500)
