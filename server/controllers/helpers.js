925a852fa1465b02976fb140d9be72679f1aa56c
Giada <alduenda.gia@gmail.com>
2022-09-29 17:45:13 -0400

    Add helpers

--- server/controllers/helpers.js
+++ server/controllers/helpers.js
@@ -1,6 +1,27 @@
 const { Sequelize } = require("sequelize");
 const db = require('../lib/db')
 
+const mapDbToApiModel = (dbModel) => {
+    return dbModel.reduce((acc, cartItem) => {
+        let resultCartItem = acc.find(innerCartItem => innerCartItem.inventoryId === cartItem.inventory_id)
+        if (!resultCartItem) {
+            resultCartItem = {
+                itemIds: [],
+                name: cartItem.name,
+                price: cartItem.price,
+                imageUrl: cartItem.image_url,
+                size: cartItem.size,
+                inventoryId: cartItem.inventory_id,
+                quantity: 0,
+            }
+            acc.push(resultCartItem)
+        }
+
+        resultCartItem.itemIds.push(cartItem.id)
+        resultCartItem.quantity++
+        return acc
+    }, [])
+}
 
 module.exports = {
     getShoppingCart: async (userId) => {
@@ -10,7 +31,14 @@ module.exports = {
         inner join product_inventory on cart.inventory_id=product_inventory.id
        inner join products on product_inventory.product_id= products.id   
         where cart.user_id = '${userId}'
-		`, { type: Sequelize.QueryTypes.SELECT })
+		`, { type: Sequelize.QueryTypes.SELECT }).then(mapDbToApiModel)
 
+    },
+
+    deleteCart: async (userId) => {
+        return db.query(`
+        delete from cart
+        where '${userId}' = user_id
+        `, { type: Sequelize.QueryTypes.DELETE })
     }
 }
